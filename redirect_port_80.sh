fd422a49053525e2d7ac7f962f9dbf4db3a02fc2
Dominik <meyerd@mytum.de>
2017-01-02 20:20:59 +0100

    fix some typos and also change ip address in dnsmasq.conf

--- redirect_port_80.sh
+++ redirect_port_80.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 HTTP_PORT=80
+DNS_PORT=53
 TV_IP=192.168.0.31
 TCP_SERVER_PORT=8888
 LOCAL_IP=192.168.0.29
 
-iptables -t nat -A PREROUTING -p tcp -d $LOCAL_IP --dport $HTTP_PORT -j DNAT --to-destination $LOCAL_P:$TCP_SERVER_PORT -s $TV_IP
+iptables -t nat -A PREROUTING -p tcp -d $LOCAL_IP --dport $HTTP_PORT -j DNAT --to-destination $LOCAL_IP:$TCP_SERVER_PORT -s $TV_IP
+iptables -t nat -A PREROUTING -p udp -d $LOCAL_IP --dport $DNS_PORT -j DNAT --to-destination $LOCAL_IP:$DNS_PORT -s $TV_IP
 sysctl net.ipv4.ip_forward=1
